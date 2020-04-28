aa000290bdc73f623182b0bc9b5cdf0a96b844f9
amusfq <amusfq@gmail.com>
2020-04-28 14:45:20 +0700

    add more feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Moment from 'react-moment';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloud, faSun, faCloudSunRain, faCloudSun, faCloudShowersHeavy, faMoon, faBolt} from '@fortawesome/free-solid-svg-icons'
+import { faCloud, faSun, faCloudSunRain, faCloudRain, faCloudSun, faCloudShowersHeavy, faMoon, faBolt, faHeart} from '@fortawesome/free-solid-svg-icons'
 
 import 'moment-timezone';
 import 'moment/locale/id';
@@ -13,6 +13,7 @@ import Rain from './Backgrounds/Rain';
 import Stars from './Backgrounds/Stars';
 
 import './css/App.css';
+import moment from 'moment-timezone';
 
 function App() {
 
@@ -24,6 +25,45 @@ function App() {
     weather: null,
   })
   const [weatherIcon, setWeatherIcon] = useState(faSun);
+  const [greeting, setGreeting] = useState('おはよう');
+  const [titleGreet, setTitleGreet] = useState(null);
+  const [titleQuotes, setTitleQuotes] = useState({'text': null, 'author': null});
+  const pagi = [
+    'Selamat pagi',
+    'Semangat pagi',
+    'Ayo semangat',
+    'Awali harimu dengan senyuman',
+    'Good morning'
+  ];
+  const siang = [
+    'Walau siang harus tetap semangat',
+    'Selamat siang',
+    'Tetap semangat',
+    'Good afternoon'
+  ];
+
+  const sore = [
+    'Penikmat senja',
+    'Good evening',
+    'Saatnya santai sejenak',
+    'Nikmati waktu istirahatmu'
+  ];
+
+  const quotes = [
+    {'text': 'Ketika kau bekerja keras dan gagal, penyesalan itu akan cepat berlalu. Berbeda dengan penyesalan ketika tidak berani mencoba.', 'author': 'Akihiko Usami (Junjou Romantica)'},
+    {'text': 'Mula-mula, kau harus merubah dirimu sendiri, atau tidak akan ada yang berubah untukmu.', 'author': 'Gintoki (Gintama)'},
+    {'text': 'Sejujurnya, hanya ada sedikit arti di balik nama yang orang tuamu berikan. Apa yang lebih berarti adalah, yang orang tuamu lakukan di balik nama itu selama kehidupannya.', 'author': 'Korosensei (Assassination Classroom)'},
+    {'text': 'Air mata adalah cara bagi hati untuk bicara, ketika bibir tak bisa mengungkapkan betapa kita merasakan sakit yang sangat.', 'author': 'Jellal Fernandes (Fairy Tail)'},
+    {'text': 'Jangan hanya berpikir tentang apa yang telah hilang darimu, kau takkan bisa mengambilnya kembali. Pikirkan apa yang masih kau miliki.', 'author': 'Jinbe (One Piece)'},
+    {'text': 'Manusia adalah makhluk yang perkasa, karena kita memiliki kemampuan untuk merubah diri kita sendiri.', 'author': 'Saitama (One Punch Man)'},
+    {'text': 'Lebih baik dibenci karena menjadi dirimu sendiri, daripada dicintai karena menjadi orang lain.', 'author': 'Hachiman (Oregairu)'},
+    {'text': 'Yare Yare Daze', 'author': 'Jotaro Kujo'},
+    {'text': 'OH MY GOD!', 'author': 'Joseph Joestar'},
+    {'text': 'HOLY SHIT!', 'author': 'Joseph Joestar'},
+    {'text': 'SON OF A BITCH!', 'author': 'Joseph Joestar'},
+    {'text': 'YOU\'RE ALREADY DEAD', 'author': 'Guts'},
+  ]
+
 
   function renderSwitch(param) {
     switch (param) {
@@ -48,6 +88,20 @@ function App() {
       const temp = response.data.current.temp;
       const weat = response.data.current.weather[0].description;
       let cuaca = null;
+      const now = moment(new Date()).format('HH');
+      let quo = quotes[Math.floor(Math.random() * quotes.length)]
+      setTitleQuotes({'text': quo.text, 'author': quo.author});
+
+      if (now < 12) {
+        setGreeting('おはよう');
+        setTitleGreet(pagi[Math.floor(Math.random() * pagi.length)]);
+      } else if (now < 18) {
+        setGreeting('こんにちは');
+        setTitleGreet(siang[Math.floor(Math.random() * siang.length)]);
+      } else {
+        setGreeting('こんばんは');
+        setTitleGreet(sore[Math.floor(Math.random() * sore.length)]);
+      }
       switch(weat) {
         case 'clear sky':
           cuaca = 'Cerah';
@@ -64,6 +118,10 @@ function App() {
         case 'rain':
           cuaca = 'Hujan'
           setWeatherIcon(faCloudSunRain);
+          setBackground(2);
+        case 'light rain':
+          cuaca = 'Hujan ringan'
+          setWeatherIcon(faCloudRain);
           setBackground(2);
         case 'thunderstrom':
           cuaca = 'Hujan badai'
@@ -85,9 +143,13 @@ function App() {
     getWeather();
   }, [])
 
+  useInterval(() => {
+    getWeather();
+  }, 1000 * 60);
 
   return (
     <>
+    <div id='title-japan'>{greeting}</div>
     <div id='weather'>
       <div id='weather-icon'>
         <FontAwesomeIcon icon={weatherIcon} size='5x'/>
@@ -97,19 +159,43 @@ function App() {
       </div>
     </div>
     <div id="jam">
-      <div id='tanggal'>
-        <Moment interval={1000} format='DD MMMM YYYY' locale='id'/>
+      <div id='jam-wrapper'>
+        <div id='tanggal'>
+          <Moment interval={1000} format='DD MMMM YYYY' locale='id'/>
+        </div>
+        <div id="waktu">
+          <Moment interval={1000} format='hh:mm:ss'/>
+        </div>
+        <div id='hari'>
+          <Moment interval={1000} format='dddd' locale='id'/>
+        </div>
       </div>
-      <div id="waktu">
-        <Moment interval={1000} format='hh:mm:ss'/>
-      </div>
-      <div id='hari'>
-        <Moment interval={1000} format='dddd' locale='id'/>
-      </div>
+      <div id='title-indo'>{titleGreet} <FontAwesomeIcon icon={faHeart} style={{'color': '#d71149'}}/></div>
+    </div>
+    <div id='quote'>
+      <div id='quote-text'>{titleQuotes.text}</div>
+      <div id='quote-author'>{titleQuotes.author}</div>
     </div>
       {renderSwitch(background)}
     </>
   );
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
 }
 
 export default App;
